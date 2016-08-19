db3983a76f5d49f51d8806a015b7ad6b5999f541
Xor Boole <mcyoung@mit.edu>
2016-08-19 19:45:58 -0400

    Refactor the entire texture file structure. Also, a few 'improvements'.

--- lightgen.sh
+++ lightgen.sh
@@ -1,9 +1,9 @@
 for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
 do 
     echo "{
-    \"parent\": \"block/day_sensor/base\",
+    \"parent\": \"block/redstone/day_sensor/base\",
     \"textures\": {
-        \"lights\": \"blocks/day_sensor/darks$i\"
+        \"lights\": \"blocks/redstone/day_sensor/lights$i\"
     }
-}" > dark$i.json
+}" > light$i.json
 done
